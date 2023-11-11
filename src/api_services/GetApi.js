f6876b4be6607c49d6acf2468c494b3ea0a730df
imran-py <imran.dtc144@gmail.com>
2023-11-11 12:21:43 +0530

    show data  into material ui table

--- src/api_services/GetApi.js
+++ src/api_services/GetApi.js
@@ -1,46 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+// for material ui
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 const GetApi = () => {
-  const [data,setData] = useState([])
+  
+  const [data,seTableCellata] = useState([])
   useEffect(()=>{
     axios.get('http://127.0.0.1:8000/students/')
-    .then(res=>{setData(res.data)})
+    .then(res=>{seTableCellata(res.data)})
     .catch(err=>{console.log(err)})
 
   })
   return (
-    <div>
+    <>
       <h3>Get By Axios Using Django api</h3>
-      {/* {
-        data.map((d,i)=>{
-          return <p key={i}>{d.f_name}</p>
-        })
-      } */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidTableCell: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{borderStyle:'solid',color:'green'}}>  
+              <TableCell align="center">First Name</TableCell>
+              <TableCell align="center">Last Name</TableCell>
+              <TableCell align="center">Stream</TableCell>
+            </TableRow>
+          </TableHead>
 
-
-      <table align='center'>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Stream</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {
-            data.map((d,i)=>(
-              <tr key={i}>
-                <td>{d.f_name}</td>
-                <td>{d.l_name}</td>
-                <td>{d.stream}</td>
-              </tr>  
-            ))
-          }
-        </tbody>
-      </table>
-    </div>
+          <TableBody>
+            {data.map((d,i)=>(
+                <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">{d.f_name}</TableCell>
+                  <TableCell align="center">{d.l_name}</TableCell>
+                  <TableCell align="center">{d.stream}</TableCell>
+                </TableRow>  
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
 
