da0a7651589e182024640df1ee9662d8b857ed9b
imran-py <imran.dtc144@gmail.com>
2023-11-11 11:26:51 +0530

    show data  into paragraph

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import GetApi from './api_services/GetApi'
 import PostApi from './api_services/PostApi'
-import PutApi from './api_services/PutApi'
-import DeleteApi from './api_services/DeleteApi'
+// import PutApi from './api_services/PutApi'
+// import DeleteApi from './api_services/DeleteApi'
 
 const App = () => {
   return (
     <div>
       <GetApi />
       <PostApi />
-      <PutApi />
-      <DeleteApi />
+      {/* <PutApi /> */}
+      {/* <DeleteApi /> */}
     </div>
   )
 }
--- src/api_services/GetApi.js
+++ src/api_services/GetApi.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const GetApi = () => {
-  
+  const [data,setData] = useState([])
   useEffect(()=>{
     axios.get('http://127.0.0.1:8000/students/')
-    .then(res=>{console.log(res.data)})
+    .then(res=>{setData(res.data)})
     .catch(err=>{console.log(err)})
 
   })
   return (
-    <>
-      
-    </>
+    <div>
+      <h3>Get By Axios Using Django api</h3>
+      {
+        data.map((d,i)=>{
+          return <p key={i}>{d.f_name}</p>
+        })
+      }
+    </div>
   )
 }
 
